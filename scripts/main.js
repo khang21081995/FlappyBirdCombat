c31dbacdd44c8835e379a209e091e29eaba289ad
MinhTuan3012 <tuanhm3012@gmail.com>
2016-07-14 19:34:19 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -15,28 +15,27 @@ window.onload = function() {
 
 //load resource
 var preload = function() {
-    FlappyCombat.game.load.image('birdDown', './images/tank_player1_down_c0_t1_s1.png'); //TODO doi anh di
-    FlappyCombat.game.load.image('birdUp', './images/tank_player1_up_c0_t1_s1.png');
-    FlappyCombat.game.load.image('birdLeft', './images/tank_player1_left_c0_t1_s1.png');
-    FlappyCombat.game.load.image('birdRight', './images/tank_player1_right_c0_t1_s1.png');
+    FlappyCombat.game.load.image('birdLeft', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdRight', './images/sprite1.png');
 
-    FlappyCombat.game.load.image('food', './images/bullet_right.png');
-    FlappyCombat.game.load.image('background', './images/images.jpg');
-
+    FlappyCombat.game.load.image('food', './images/Nintendo-Button-B.png');
+    FlappyCombat.game.load.image('barrie', './images/big_explosion_5.png');
+    FlappyCombat.game.load.image('background', './images/map.png') ;
 }
 var bird;
 var create = function() {
+    FlappyCombat.game.add.tileSprite(0, 0, 6000, 1000, "background");
     FlappyCombat.game.physics.startSystem(Phaser.Physics.ARCADE);
     FlappyCombat.keyboard = FlappyCombat.game.input.keyboard;
     FlappyCombat.game.physics.arcade.gravity.y = 10;
     FlappyCombat.birdGroup = FlappyCombat.game.add.physicsGroup();
     FlappyCombat.foodGroup = FlappyCombat.game.add.physicsGroup();
-    FlappyCombat.game.world.setBounds(0, 0, 10000, 1000);
+    FlappyCombat.game.world.setBounds(0, 0, 6000, 1000);
     bird = new Bird(500, 500, FlappyCombat.birdGroup, 1);
     FlappyCombat.inputController = new InputController(FlappyCombat.keyboard, bird);
     FlappyCombat.game.camera.follow(bird.sprite);
-    for (var i = 0; i < 100; i++) {
-        FlappyCombat.game.add.sprite(FlappyCombat.game.world.randomX, FlappyCombat.game.world.randomY, 'background');
+    for (var i = 0; i < 20; i++) {
+        FlappyCombat.game.add.sprite(FlappyCombat.game.world.randomX, FlappyCombat.game.world.randomY, 'barrie');
     }
 
 
