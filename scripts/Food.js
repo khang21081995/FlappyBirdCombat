0a3aa307ce1bf7ba88adf6c6e664f167ec96ac80
khang21081995 <khang21081995@gmail.com>
2016-07-14 22:59:54 +0700

    eo biet lam them duoc cai gi roi nhi

--- scripts/Food.js
+++ scripts/Food.js
@@ -1,8 +1,16 @@
 class Food {
-    constructor(x, y, group) {
-        this.sprite = group.create(x, y, 'food');
+    constructor(x, y, group, typeOfFood) {
+
+        if (typeOfFood == 1) {
+            this.sprite = group.create(x, y, 'food');
+            this.sprite.score = 1;
+        } else {
+            this.sprite = group.create(x, y, 'food2');
+            this.sprite.score = 5;
+        }
         FlappyCombat.game.physics.arcade.enable(this.sprite);
         this.sprite.anchor.set(0.5, 0.5);
-        this.sprite.score = 1;
+        this.sprite.typeOfFood = typeOfFood;
+
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -15,27 +15,38 @@ window.onload = function() {
 
 //load resource
 var preload = function() {
-    FlappyCombat.game.load.image('birdLeft', './images/sprite1.png');
-    FlappyCombat.game.load.image('birdRight', './images/sprite1.png');
-
-    FlappyCombat.game.load.image('food', './images/Nintendo-Button-B.png');
+    FlappyCombat.game.load.image('birdLeftlv1', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdRightlv1', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdLeftlv2', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdRightlv2', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdLeftlv3', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdRightlv3', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdLeftlv4', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdRightlv4', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdLeftlv5', './images/sprite1.png');
+    FlappyCombat.game.load.image('birdRightlv5', './images/sprite1.png');
+    FlappyCombat.game.load.image('food', './images/wall_steel.png');
+    FlappyCombat.game.load.image('food2', './images/Nintendo-Button-B.png');
     FlappyCombat.game.load.image('barrie', './images/big_explosion_5.png');
-    FlappyCombat.game.load.image('background', './images/map.png') ;
+    FlappyCombat.game.load.image('background', './images/map.png');
 }
 var bird;
+var numberOfFood = 0;
 var create = function() {
     FlappyCombat.game.add.tileSprite(0, 0, 6000, 1000, "background");
     FlappyCombat.game.physics.startSystem(Phaser.Physics.ARCADE);
     FlappyCombat.keyboard = FlappyCombat.game.input.keyboard;
-    FlappyCombat.game.physics.arcade.gravity.y = 10;
+    //FlappyCombat.game.physics.arcade.gravity.y = 10;
     FlappyCombat.birdGroup = FlappyCombat.game.add.physicsGroup();
     FlappyCombat.foodGroup = FlappyCombat.game.add.physicsGroup();
+    FlappyCombat.barieGroup = FlappyCombat.game.add.physicsGroup();
+
     FlappyCombat.game.world.setBounds(0, 0, 6000, 1000);
     bird = new Bird(500, 500, FlappyCombat.birdGroup, 1);
     FlappyCombat.inputController = new InputController(FlappyCombat.keyboard, bird);
     FlappyCombat.game.camera.follow(bird.sprite);
     for (var i = 0; i < 20; i++) {
-        FlappyCombat.game.add.sprite(FlappyCombat.game.world.randomX, FlappyCombat.game.world.randomY, 'barrie');
+        new Barrie(FlappyCombat.game.world.randomX, FlappyCombat.game.world.randomY, FlappyCombat.barieGroup);
     }
 
 
@@ -43,7 +54,11 @@ var create = function() {
 
 
 var update = function() {
-    new Food(FlappyCombat.game.world.randomX, FlappyCombat.game.world.randomY, FlappyCombat.foodGroup)
+    if (numberOfFood < 100) {
+        new Food(FlappyCombat.game.world.randomX, FlappyCombat.game.world.randomY, FlappyCombat.foodGroup,Math.round(Math.random()));
+        numberOfFood ++;
+    }
+
     FlappyCombat.game.physics.arcade.overlap(
         FlappyCombat.birdGroup,
         FlappyCombat.foodGroup,
@@ -52,13 +67,48 @@ var update = function() {
         this
     );
 
+    FlappyCombat.game.physics.arcade.overlap(
+        FlappyCombat.birdGroup,
+        FlappyCombat.barieGroup,
+        onBarieMeetBird,
+        null,
+        this
+    );
+
+    FlappyCombat.game.physics.arcade.overlap(
+        FlappyCombat.foodGroup,
+        FlappyCombat.barieGroup,
+        onBarieMeetFood,
+        null,
+        this
+    );
+
     if (FlappyCombat.inputController) FlappyCombat.inputController.update();
 }
 
+/*
+ *  HELPER FUNCTIONS
+ */
+
+
+/*
+ * PHYSICS EVENTS
+ */
 
 var onFoodMeetBird = function(birdSprite, foodSprite) {
 
     birdSprite.score += foodSprite.score;
-    console.log(birdSprite.score);
     foodSprite.kill();
+    numberOfFood--;
+    //foodSprite.destroy();
+}
+
+var onBarieMeetBird = function(birdSprite, barrieSprite) {
+    birdSprite.kill();
+    //birdSprite.destroy();
+}
+
+var onBarieMeetFood = function(foodSprite, barrieSprite) {
+  foodSprite.kill();
+  numberOfFood--;
 }
